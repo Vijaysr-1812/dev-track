5519664162da80c6f5b6de724d9ad9208a5fc0f4
Vijaysr-1812 <147262423+Vijaysr-1812@users.noreply.github.com>
2024-04-20 09:07:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Navbar from './Components/navbar/Navbar';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import  Shop  from './Pages/Shop';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import ShopCatergory from './Pages/ShopCatergory';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<shop/>}/>
+      <Route path='/mens' element={<ShopCatergory category="mens"/>}/>
+      <Route path='/womens' element={<ShopCatergory category="womens"/>}/>
+      <Route path='/kids' element={<ShopCatergory category="kids"/>}/>
+      <Route path='/product' element={<Product/>}>
+      <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>   
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/ShopCatergory.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const ShopCatergory = () => {
+  return (
+    <div>ShopCatergory</div>
+  )
+}
+export default ShopCatergory;
